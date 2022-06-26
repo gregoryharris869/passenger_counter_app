1d86fa38da4121fc07948fdcd254d9d67a96cda5
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-06-25 20:03:18 -0500

    started JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict";
 //
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// function increment() {
+//   console.log("Button was clicked");
+// }
+
+
