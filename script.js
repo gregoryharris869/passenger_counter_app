90d07f00ed1581dadfca18489185c1169c7575c4
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-06-30 15:27:44 -0500

    Started JavaScript

--- script.js
+++ script.js
@@ -5,8 +5,21 @@
 // increment the count variable when button is clicked
 // change the count-el in the HTML to reflect the new count
 
+// let count = 0;
+// let countEl = document.getElementById("count-el");
+
+// // console.log(countEl);
+
 // function increment() {
-//   console.log("Button was clicked");
+//   //   console.log("clicked");
+//   count++;
+//   countEl.innerText = count;
 // }
 
+// function save() {
+//   console.log(count);
+// }
+let username = "per";
+let message = "You have three new notifications";
 
+console.log(message + "," + username + "!");
